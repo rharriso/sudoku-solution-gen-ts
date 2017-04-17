181e0ef9e63c5db7c8382467da83ee65cf455c6c
Ross Harrison <rtharrison86@gmail.com>
2017-04-16 21:48:18 -0400

    convert to 1D array

--- main.ts
+++ main.ts
@@ -1,121 +1,141 @@
 import {
-    sample as _sample,
-    times as _times,
-    sortedUniqBy as _sortedUniqBy
+  sample as _sample,
+  times as _times,
+  sortedUniqBy as _sortedUniqBy
 } from 'lodash';
 
+let total = 81;
 let size = 9;
 let third = 3;
+let validValues = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 /**
- * 
- */
+*
+*/
 interface coord {
-    i: number;
-    j: number;
+  i: number;
+  j: number;
 };
 
 /**
- * 
- */
+*
+*/
 class SudokuCell {
-    value: Number = 0;
-    position: coord;
-    neighbors: Array<coord> = [];
+  value: Number = 0;
+  position: coord;
+  neighbors: Array<coord> = [];
 
-    constructor (position: coord) {
-        this.position = position;
-        this.value = 0;
+  constructor (position: coord) {
+    this.position = position;
+    this.value = 0;
 
-        // identify cell neighbors
-        const iBase = Math.floor(position.i / third) * third;
-        const jBase = Math.floor(position.j / third) * third;
-        const cellNeighbors = [];
-        _times(size, (index: number) => {
-            const i = Math.floor(index / third) + iBase;
-            const j = (index % third) + jBase;
+    // identify cell neighbors
+    const iBase = Math.floor(position.i / third) * third;
+    const jBase = Math.floor(position.j / third) * third;
+    const cellNeighbors = [];
+    _times(size, (index: number) => {
+      const i = Math.floor(index / third) + iBase;
+      const j = (index % third) + jBase;
 
-            if(i !== position.i || j !== position.j) {
-                cellNeighbors.push({i, j});
-            } 
-        });
+      if(i !== position.i || j !== position.j) {
+        cellNeighbors.push({i, j});
+      }
+    });
 
 
-        // identify row neighbors
-        const rowNeighbors = _times(size, (i) => {
-            return {i: i, j: position.j};
-        });
-        rowNeighbors.splice(position.i, 1);
+    // identify row neighbors
+    const rowNeighbors = _times(size, (i) => {
+      return {i: i, j: position.j};
+    });
+    rowNeighbors.splice(position.i, 1);
 
-        // identify col neighbors
-        const colNeighbors = _times(size, (j) => {
-            return {i: position.i, j: j};
-        });
-        colNeighbors.splice(position.j, 1);
+    // identify col neighbors
+    const colNeighbors = _times(size, (j) => {
+      return {i: position.i, j: j};
+    });
+    colNeighbors.splice(position.j, 1);
 
-        // all neighbors
-        this.neighbors = _sortedUniqBy([
-            ...rowNeighbors,
-            ...colNeighbors,
-            ...cellNeighbors
-        ], (c) => `${c.i}:${c.j}`);
-    }
+    // all neighbors
+    this.neighbors = _sortedUniqBy([
+      ...rowNeighbors,
+      ...colNeighbors,
+      ...cellNeighbors
+    ], (c) => `${c.i}:${c.j}`);
+  }
 }
 
 
 /**
- * 
- */
+*
+*/
 class SudokuBoard {
-    private cells: SudokuCell[][];
+  private cells: SudokuCell[];
 
-    constructor (){
-        this.cells = _times(size, (i) =>
-            _times(size, (j) => new SudokuCell({i, j}))
-        );
-    }
+  constructor (){
+      this.cells = _times(total, (index) => {
+      const position = this.resolveIndex(index);
+      return new SudokuCell(position);
+    });
+  }
 
-    print() {
-        const line = '--------------------------';
-        console.log(line)
-        _times(size, (i) => {
-            _times(size, (j) => {
-                if ( j % third === 0) {
-                    process.stdout.write('| ');
-                }
-                process.stdout.write(`${this.cells[i][j].value} `);
-                new SudokuCell({i, j});
-            });
-            process.stdout.write('|\n');
-            if ( i % third === 2) {
-                console.log(line);
-            }
-        });
-    }
+  print() {
+    const line = '--------------------------';
+    console.log(line)
+    _times(size, (i) => {
+      _times(size, (j) => {
+        const cell = this.cells[this.resolvePosition({i, j})];
 
-    /**
-     * 
-     */
-    sample(): SudokuCell {
-        return _sample(_sample(this.cells));
-    }
+        if ( j % third === 0) {
+          process.stdout.write('| ');
+        }
+        process.stdout.write(`${cell.value} `);
+      });
+      process.stdout.write('|\n');
+      if ( i % third === 2) {
+        console.log(line);
+      }
+    });
+  }
 
-    /**
-     * 
-     */
-    at(c: coord): SudokuCell {
-        return this.cells[c.i][c.j];
+  fill () {
+    if(!this.doFillCells(this.cells)) {
+      console.error('Unable to fill board');
     }
-    
-    set(c: coord, v: number) {
-        this.cells[c.i][c.j].value = v;
+  }
+
+  doFillCells(cells: SudokuCell[]) {
+    const cell = cells[0];
+
+    return false;
+  }
+
+  resolveIndex(index: number) : coord {
+    return {
+      i: Math.floor(index / size),
+      j: (index % size),
     }
+  }
+
+  resolvePosition(position: coord) : number {
+    return position.i * size + position.j;
+  }
+
+  /**
+  *
+  */
+  at(c: coord): SudokuCell {
+    return this.cells[c.i][c.j];
+  }
+
+  atIndex(index: number): SudokuCell {
+    return this.cells[index];
+  }
+
+  set(c: coord, v: number) {
+    this.cells[c.i][c.j].value = v;
+  }
 }
 
 let board = new SudokuBoard();
-let cell = board.sample();
-
-cell.neighbors.map((coord) => board.set(coord, 7));
-board.set(cell.position, 5);
 
 board.print();
